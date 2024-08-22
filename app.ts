bb95ae8c69cdb3f12bfeaef524d62e1ff18ab1c7
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-22 20:48:33 +0200

    update main file structure

--- app.ts
+++ app.ts
@@ -1,18 +1,23 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import createError, { HttpError } from 'http-errors';
 import dotenv from 'dotenv';
 import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
 import expressSession from 'express-session';
+import passport from 'passport';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { PrismaClient } from '@prisma/client';
 dotenv.config(); // to be removed?
+
+import indexRouter from './routes/indexRouter';
+import userRouter from './routes/userRouter';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(expressLayouts);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -33,8 +38,31 @@ app.use(
   })
 );
 
-app.get('/', (req: Request, res: Response) => {
-  res.render('index');
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+// app.use('/folders', folderRouter);
+// app.use('/files', fileRouter);
+// app.use('/share', shareRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', { title: 'Error' });
 });
 
 app.listen(port, () => {
