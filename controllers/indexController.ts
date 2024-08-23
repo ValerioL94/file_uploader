e8287ec58b1eac323b7ea2058e46ef55d478e18d
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-23 16:59:55 +0200

    update index controller, router

--- controllers/indexController.ts
+++ controllers/indexController.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client';
 import asyncHandler from 'express-async-handler';
 import { body, validationResult } from 'express-validator';
 
-export const index = asyncHandler(async (req, res, next) => {
-  res.render('index', { title: 'Homepage' });
+export const index_get = asyncHandler(async (req, res, next) => {
+  res.render('index', { title: 'Homepage', user: req.user });
 });
--- routes/indexRouter.ts
+++ routes/indexRouter.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const indexRouter = express.Router();
-import { index } from '../controllers/indexController';
+import { index_get } from '../controllers/indexController';
 
-indexRouter.get('/', index);
+indexRouter.get('/', index_get);
 
 export default indexRouter;
