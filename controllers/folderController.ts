d250e28e3748adfb1048e6b1176a98e481a5977c
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-24 20:19:10 +0200

    update folder router and controller

--- controllers/folderController.ts
+++ controllers/folderController.ts
@@ -1,16 +1,19 @@
 import asyncHandler from 'express-async-handler';
-import { body, validationResult } from 'express-validator';
-import passport from 'passport';
-import bcrypt from 'bcryptjs';
 import prisma from '../prisma/client';
 
 export const folders_get = asyncHandler(async (req, res, next) => {
-  const folders = await prisma.folder.findMany();
+  const folders = await prisma.folder.findMany({
+    orderBy: { updatedAt: 'desc' },
+  });
   res.render('folders', { title: 'Your folders', user: req.user, folders });
 });
 
 export const folder_create_get = asyncHandler(async (req, res, next) => {
-  res.render('folder_form', { title: 'New folder', user: req.user });
+  res.render('folder_form', {
+    title: 'New folder',
+    user: req.user,
+    type: 'create',
+  });
 });
 
 export const folder_create_post = asyncHandler(async (req, res, next) => {
@@ -19,6 +22,36 @@ export const folder_create_post = asyncHandler(async (req, res, next) => {
       title: req.body.title,
       user: { connect: { id: req.user.id } },
     },
+  });
+  res.redirect('/folders');
+});
+
+export const folder_edit_get = asyncHandler(async (req, res, next) => {
+  const folder = await prisma.folder.findUnique({
+    where: {
+      id: req.params.folderId,
+    },
+  });
+  res.render('folder_form', {
+    title: 'Update folder',
+    user: req.user,
+    type: 'update',
+    folder,
+  });
+});
+
+export const folder_edit_post = asyncHandler(async (req, res, next) => {
+  const newTitle = req.body.title;
+  await prisma.folder.update({
+    where: { id: req.params.folderId },
+    data: { title: newTitle },
+  });
+  res.redirect('/folders');
+});
+
+export const folder_delete_get = asyncHandler(async (req, res, next) => {
+  await prisma.folder.delete({
+    where: { id: req.params.folderId },
   });
   res.redirect('/folders');
 });
--- routes/folderRouter.ts
+++ routes/folderRouter.ts
@@ -5,11 +5,18 @@ import {
   folders_get,
   folder_create_get,
   folder_create_post,
+  folder_edit_get,
+  folder_edit_post,
+  folder_delete_get,
 } from '../controllers/folderController';
 
 folderRouter.use(isAuth);
 folderRouter.get('/', folders_get);
-folderRouter.get('/create', folder_create_get);
-folderRouter.post('/create', folder_create_post);
+folderRouter.route('/create').get(folder_create_get).post(folder_create_post);
+folderRouter
+  .route('/:folderId/edit')
+  .get(folder_edit_get)
+  .post(folder_edit_post);
+folderRouter.get('/:folderId/delete', folder_delete_get);
 
 export default folderRouter;
