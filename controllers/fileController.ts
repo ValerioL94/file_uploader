8ed1b7e303fcd3e1f694ced657eea98a3f9c55f9
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-25 20:06:28 +0200

    add file upload controller

--- controllers/fileController.ts
+++ controllers/fileController.ts
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import prisma from '../prisma/client';
-import multer from 'multer';
+import { upload, getURI } from '../utils/multer_config';
+import { handleUpload } from '../utils/cloudinary_config';
 
 export const file_list_get = asyncHandler(async (req, res, next) => {
   const folder = await prisma.folder.findUnique({
@@ -19,13 +20,37 @@ export const file_upload_get = asyncHandler(async (req, res, next) => {
   res.render('file_form', { title: 'File upload', user: req.user });
 });
 
-const upload = multer({ dest: 'uploads/' });
 export const file_upload_post = [
   upload.single('uploaded_file'),
   asyncHandler(async (req, res, next) => {
-    console.log(req.file);
-    console.log(req.body.file_name);
-    res.redirect('/folders');
+    const folder = await prisma.folder.findUnique({
+      where: { id: req.params.folderId },
+    });
+    if (!req.file) {
+      return res.render('file_form', {
+        title: 'File upload',
+        user: req.user,
+        error: 'No file selected',
+      });
+    }
+    if (req.file && req.file.size > 1048576) {
+      return res.render('file_form', {
+        title: 'File upload',
+        user: req.user,
+        error: 'File size exceeded',
+      });
+    }
+    const dataURI = getURI(req.file.buffer, req.file.mimetype);
+    const result = await handleUpload(dataURI, folder!.title);
+    await prisma.file.create({
+      data: {
+        title: req.body.file_name,
+        size: result.bytes,
+        url: result.secure_url,
+        folder: { connect: { id: folder!.id } },
+      },
+    });
+    res.redirect(`/folders/${folder!.id}/files`);
   }),
 ];
 
